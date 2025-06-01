f253bff108081f3c1d851a94cbdef638bf1a0517
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-06-01 10:46:52 -0400

    Enable parallel voxel-wise loops

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,34 @@
 `%||%` <- function(a, b) {
   if (!is.null(a)) a else b
 }
+
+#' Internal parallel lapply helper
+#'
+#' Applies a function over a set of elements using parallel backends
+#' when requested. The function first attempts to use
+#' \pkg{future.apply} with a multisession plan, falling back to
+#' \code{parallel::mclapply} on Unix-like systems. If neither backend is
+#' available or \code{n_jobs = 1}, a regular \code{lapply} is used.
+#'
+#' @param X Vector or list of elements to iterate over.
+#' @param FUN Function to apply to each element.
+#' @param n_jobs Number of parallel workers. Set to 1 for sequential
+#'   execution.
+#' @return A list of results from applying \code{FUN}.
+#' @keywords internal
+.parallel_lapply <- function(X, FUN, n_jobs = 1) {
+  if (n_jobs > 1) {
+    if (requireNamespace("future.apply", quietly = TRUE)) {
+      oplan <- future::plan(future::multisession, workers = n_jobs)
+      on.exit(future::plan(oplan), add = TRUE)
+      future.apply::future_lapply(X, FUN)
+    } else if (.Platform$OS.type != "windows" &&
+               requireNamespace("parallel", quietly = TRUE)) {
+      parallel::mclapply(X, FUN, mc.cores = n_jobs)
+    } else {
+      lapply(X, FUN)
+    }
+  } else {
+    lapply(X, FUN)
+  }
+}
