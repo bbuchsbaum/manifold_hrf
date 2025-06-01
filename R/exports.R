29cd04d8b76cb51907ad97dcef38da4102bc9436
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-06-01 13:45:26 -0400

    Add simplified mhrf_lss interface

--- R/exports.R
+++ R/exports.R
@@ -6,3 +6,8 @@ create_hrf_manifold <- create_hrf_manifold
 
 #' @export
 get_preset_params <- get_preset_params
+#' @export
+mhrf_lss <- mhrf_lss
+
+#' @export
+mhrf_lss_parameters <- mhrf_lss_parameters
